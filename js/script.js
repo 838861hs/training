58469225fc142cbc5b46cb3f8ed8833638cd5e98
838861hs <suzuki838861@gmail.com>
2017-07-09 12:31:54 +0900

    切り替え実装

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 $(function(){
+  $('.tab_name li a').on('click',function(){
+//タブのアクティブ
+    $('.tab_name li').removeClass('active');
+    var index = $(this).parent('li').index();
+    $('.tab_name li').eq(index).addClass('active');
 
+
+//写真の変更
+    if(index == 0){
+      $('.content_box li a img').attr({src:'img/sf.jpg'})
+    }else if(index == 1){
+      $('.content_box li a img').attr({src:'img/action.jpg'})
+    }else if(index == 2){
+      $('.content_box li a img').attr({src:'img/sf.jpg'})
+    }else if(index == 3){
+      $('.content_box li a img').attr({src:'img/romance.jpg'})
+    }else if(index == 4){
+      $('.content_box li a img').attr({src:'img/comedy.jpg'})
+    }
+});
 });
