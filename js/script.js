3345e4763998f3f7d54994691d3d0debc343a129
838861hs <suzuki838861@gmail.com>
2017-07-09 12:54:38 +0900

    切り替え実装の修正

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
 $(function(){
+
+var load_src = ['img/film1.jpg',
+                'img/film2.jpg',
+                'img/film3.jpg',
+                'img/film4.jpg',
+                'img/film5.jpg',
+                'img/film6.jpg'
+                ]
+
+
+for (var i = 0;i < 6; i++) {
+  var target = $('.content_box li').eq(i).find('img');
+  target.attr({src:'img/film' + [i + 1] +'.jpg'})
+}
+
+
+
+
+
+console.log(load_src[0])
   $('.tab_name li a').on('click',function(){
 //タブのアクティブ
     $('.tab_name li').removeClass('active');
     var index = $(this).parent('li').index();
     $('.tab_name li').eq(index).addClass('active');
-
-
 //写真の変更
     if(index == 0){
-      $('.content_box li a img').attr({src:'img/sf.jpg'})
+    for (var i = 0;i < 6; i++) {
+      var target = $('.content_box li').eq(i).find('img');
+     target.attr({src:'img/film' + [i + 1] +'.jpg'})
+    }
     }else if(index == 1){
       $('.content_box li a img').attr({src:'img/action.jpg'})
     }else if(index == 2){
