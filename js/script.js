25a6bd2b8b5be7606ac04c4400950507c53532ab
838861hs <suzuki838861@gmail.com>
2017-07-10 20:30:21 +0900

    issue#2の完成コミット

--- js/script.js
+++ js/script.js
@@ -1,43 +1,14 @@
 $(function(){
-
-var load_src = ['img/film1.jpg',
-                'img/film2.jpg',
-                'img/film3.jpg',
-                'img/film4.jpg',
-                'img/film5.jpg',
-                'img/film6.jpg'
-                ]
-
-
-for (var i = 0;i < 6; i++) {
-  var target = $('.content_box li').eq(i).find('img');
-  target.attr({src:'img/film' + [i + 1] +'.jpg'})
-}
-
-
-
-
-
-console.log(load_src[0])
   $('.tab_name li a').on('click',function(){
 //タブのアクティブ
     $('.tab_name li').removeClass('active');
     var index = $(this).parent('li').index();
     $('.tab_name li').eq(index).addClass('active');
-//写真の変更
-    if(index == 0){
-    for (var i = 0;i < 6; i++) {
-      var target = $('.content_box li').eq(i).find('img');
-     target.attr({src:'img/film' + [i + 1] +'.jpg'})
-    }
-    }else if(index == 1){
-      $('.content_box li a img').attr({src:'img/action.jpg'})
-    }else if(index == 2){
-      $('.content_box li a img').attr({src:'img/sf.jpg'})
-    }else if(index == 3){
-      $('.content_box li a img').attr({src:'img/romance.jpg'})
-    }else if(index == 4){
-      $('.content_box li a img').attr({src:'img/comedy.jpg'})
-    }
-});
+//映像の切り替え
+$('.content_box li a img').removeClass('active');
+   $('.content_box li').each(function(){
+    $(this).find('img').eq(index).addClass('active')
+   });
+
+  });
 });
