f62d469ae18d87a4decae66efe13af8f27b1c192
838861hs <suzuki838861@gmail.com>
2017-07-15 16:33:17 +0900

    7/15　背景画像の動きの修正

--- js/script.js
+++ js/script.js
@@ -1,53 +1,64 @@
 $(function(){
-  var $mv_bg =$('.mv_bg'),
-  current_index = 0;
-
-  $mv_bg.each(function(i){
-    $(this).css({left: 100 *  i + '%'});
-  });
-
-function goNext(index){
-  $('.bg_box').animate({left: -100 * index + '%'})
+var slider_parts = {
+  $mv_bg:$('.mv_bg'),
+  $prev :$('.prev'),
+  $next :$('.next'),
+  current_index: 0,
+//背景画像の横並び
+  bg_horizontal: $('.mv_bg').each(function(i){$(this).css({left: 100 *  i + '%'}); }),
+//次の背景に移動
+  goNext: function(index){$('.bg_box').animate({left: -100 * index + '%'}) },
 }
 
 
-$('.next').on('click',function(){
-  if (current_index == 2) {
-    current_index = 0;
-    goNext(current_index)
-  }else{
-    current_index++;
-    goNext(current_index)
-  }
-  console.log(current_index);
-});
+//次をクリックした
+slider_parts.nextMove = function(){
+    if (slider_parts.current_index == 2) {
+      slider_parts.current_index = 0;
+      slider_parts.goNext(slider_parts.current_index);
+    }else{
+         slider_parts.current_index++;
+         slider_parts.goNext(slider_parts.current_index);
+         console.log(slider_parts.current_index)
+      }
+}
 
+slider_parts.prevMove = function(){
+    if (slider_parts.current_index == 0) {
+      slider_parts.current_index = 2;
+      slider_parts.goNext(slider_parts.current_index);
+    }else{
+         slider_parts.current_index--;
+         slider_parts.goNext(slider_parts.current_index);
+         console.log(slider_parts.current_index)
+      }
+}
 
-$('.prev').on('click',function(){
-  if (current_index == 0) {
-    current_index = 2;
-    goNext(current_index);
-  }else{
-    current_index--;
-    goNext(current_index);
-  }
-  console.log(current_index)
-});
+slider_parts.handleEvent = function() {
+    var self = this;
+    
+    this.$next.on('click', function(){
+        self.nextMove();
+    });
+    
+    this.$prev.on('click', function(){
+        self.prevMove();
+    });
+}
 
+slider_parts.handleEvent();
 
   $('.tab_name li a').on('click',function(){
 //タブのアクティブ
-    $('.tab_name li').removeClass('active');
-    var index = $(this).parent('li').index();
-    $('.tab_name li').eq(index).addClass('active');
+  $('.tab_name li').removeClass('active');
+  var index = $(this).parent('li').index();
+  $('.tab_name li').eq(index).addClass('active');
 //映像の切り替え
   var current_id = $(this).parent('li').attr('id');
   console.log(current_id);
   $('.photo_content').removeClass('active');
   $('.'+current_id).addClass('active');
   });
-
-
 
 
 
