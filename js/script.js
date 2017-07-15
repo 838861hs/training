0c5d9ef4d0c0036fa5025c9bcd2462fa035bf425
838861hs <suzuki838861@gmail.com>
2017-07-15 23:20:49 +0900

    スクロールヘッダーの仕様の修正

--- js/script.js
+++ js/script.js
@@ -2,24 +2,13 @@ $(function(){
 
   $(window).scroll(function(){
     var top = $(this).scrollTop();
-    console.log(top);
-
-
-function hide_header(){
-  if (100 < top ) {
-    $('header').stop().css('top','-100%');
-  }else{
-    $('header').stop().css('top','0');
-  }
-}
 
 function show_header(){
-  if (400 < top) {
-    $('header').stop().css('position','fixed');
-    $('header').stop().css('top','0');
+  if (640 < top) {
+    $('header').css('position','fixed');
+    $('header').css('top','0');
   }else{
-    $('header').stop().css('position','-100%');
-    hide_header();
+    $('header').css('position','absolute');
   }
 }
 
