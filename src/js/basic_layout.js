b94114e70324a0c97a214ea6725f866dd8ef48bd
838861hs <philoaki0@gmail.com>
2022-08-11 21:32:00 +0900

    feat:スライダーをオートプレイ追加

--- src/js/basic_layout.js
+++ src/js/basic_layout.js
@@ -1,8 +1,11 @@
 import "@scss/basic_layout.scss";
 import "swiper/css";
-import Swiper from "swiper";
+import Swiper from "swiper/bundle";
 
 /* eslint-disable */
 const swiper = new Swiper(".swiper", {
   loop: true,
+  autoplay: {
+    delay: 2000,
+  },
 });
